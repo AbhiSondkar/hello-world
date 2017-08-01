f808bb8d62210b73d0b4807626b8e4018eab68e2
AbhiSondkar <abhisondkar9999@gmail.com>
2017-08-01 18:43:11 +0530

    Add files via upload

---
+++ hellojava.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class wow{
+public static void main(String args[])
+{
+	
+	System.out.println("Hello World");
+}}
+
+
+
+/*This is neccesary because main() is called by the JVM before any objects are made. 
+Since it is static it can be directly invoked via the class.
+Similarly, we use static sometime for user defined methods so that we need not to make objects. 
+void indicates that the main() method being declared does not return a value*/
